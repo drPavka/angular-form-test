6fd4bd9ec69086cead7172b5c3e8377859f3f020
Pavlo Dubenko <d.pavka@gmail.com>
2020-08-16 22:34:44 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mouse-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mouselive-frontend-test';
+}
